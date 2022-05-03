f40e728ea3547d31b5c4e32ad15641050de9afe3
3B9ACA00 <git3B9ACA00@gmail.com>
2022-05-03 22:45:33 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+up: docker-up
+init: docker-down-clear docker-pull docker-build docker-up app-init
+test: app-test
+
+docker-up:
+	docker-compose up -d
+
+docker-down:
+	docker-compose down --remove-orphans
+
+docker-down-clear:
+	docker-compose down -v --remove-orphans
+
+docker-pull:
+	docker-compose pull
+
+docker-build:
+	docker-compose build
+
+app-init: app-composer-install app-composer-update
+
+app-composer-install:
+	docker-compose run --rm app-php-cli composer install
+
+app-composer-update:
+	docker-compose run --rm app-php-cli composer update
+
+app-test:
+	docker-compose run --rm app-php-cli php bin/phpunit
+
+symfony-check:
+	docker-compose run --rm app-php-cli symfony check:requirements
+
+sec-check:
+	docker-compose run --rm app-php-cli symfony check:security
---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @throws Exception
+     */
+    #[Route('/')]
+    public function home(): Response
+    {
+        return new Response(
+            '<!doctype html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Document</title>
+                </head>
+                <body>
+                    <h1>Hello Project!</h1>
+                </body>
+            </html>'
+        );
+    }
+}
+
